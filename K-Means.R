acbaff4a00c187719474a6a3c28162eca24cd8be
abderrahmaneouazzani <abderrahmaneouazzani@gmail.com>
2020-11-02 00:37:40 +0100

    final

---
+++ K-Means.R
@@ -0,0 +1,26 @@
+# Install pre-requisite packages #
+
+install.packages("stats")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("ggfortify")
+
+# Load required librairies #
+
+library(stats)
+library(dplyr)
+library(ggplot2)
+library(ggfortify)
+
+# Reading the data #
+
+data<-read.csv("places2.csv")
+
+# K-Means #
+
+km = kmeans(data,3)
+
+autoplot(km,data,frame=TRUE)
+
+write.csv(km$cluster, 'LIST')
+
